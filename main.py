1dd9c7e22c5bc418a99334cfd31501c7cb620db1
Bella3009 <bellasara13@gmail.com>
2023-06-16 22:31:19 +0200

    Change the image to grey scale and recognising when there is movement by showing it in white.

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import cv2
 import time
 
 video = cv2.VideoCapture(0)
+time.sleep(1)
+
+first_frame = None
 
 while True:
-    time.sleep(1)
+    # Set the camera
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grey_frame_gau = cv2.GaussianBlur(grey_frame, (21, 21), 0)
+
+    # Check if the first image captured by the camera so to store it
+    if first_frame is None:
+        first_frame = grey_frame_gau
+
+    # Check if the current image is the same as the first image
+    delta_frame = cv2.absdiff(first_frame, grey_frame)
+    cv2.imshow("My video", delta_frame)
 
     key = cv2.waitKey(1)
 
